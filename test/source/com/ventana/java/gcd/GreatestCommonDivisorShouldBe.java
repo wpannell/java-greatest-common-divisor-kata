96f5d2d91ed8f38f82a65bcef79a3b5ad0d1c957
wpannell <wil.pannell@gmail.com>
2013-08-26 19:28:02 -0700

    passing test: twoWhenFactorsAre2And4

--- test/source/com/ventana/java/gcd/GreatestCommonDivisorShouldBe.java
+++ test/source/com/ventana/java/gcd/GreatestCommonDivisorShouldBe.java
@@ -10,7 +10,7 @@ public class GreatestCommonDivisorShouldBe {
   private int gcdOf(int... factors) {
     if(factors[0] == 1 || factors[1] == 1) return 1;
     if(factors[0] == factors[1]) return factors[0];
-    if(factors[1] % factors[0] == 1) return 1;
+    if(factors[1] % factors[0] == 0) return factors[0];
     return gcdOf(factors[1] % factors[0], factors[0]);
   }
 
