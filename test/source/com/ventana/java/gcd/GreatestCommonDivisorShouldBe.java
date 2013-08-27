5bcb7807bd382346d1cdb5072efc8ed8b54e2104
wpannell <wil.pannell@gmail.com>
2013-08-26 18:35:35 -0700

    failing test: oneWhenFactorsAre1And1

--- test/source/com/ventana/java/gcd/GreatestCommonDivisorShouldBe.java
+++ test/source/com/ventana/java/gcd/GreatestCommonDivisorShouldBe.java
@@ -1,4 +1,18 @@
 package com.ventana.java.gcd;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GreatestCommonDivisorShouldBe {
+  private int gcdOf(int... factors) {
+    return 0;
+  }
+
+  @Test
+  public void oneWhenFactorsAre1And1() {
+    assertThat("gcdOf(1, 1)", gcdOf(1, 1), is(equalTo(1)));
+  }
 }
